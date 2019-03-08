65ec8e7e7bb771ea63b2421c8539bf7636183045
Mohtasim Howlader <mhowlader@stuy.edu>
2019-03-08 01:01:23 -0500

    added hw

--- 08_mongosite/app.py
+++ 08_mongosite/app.py
@@ -1,9 +1,33 @@
-from flask import Flask, render_template, request
+# Team Mongolia (Mohtasim Howlader, Maryann Foley, Derek Chan
+# SoftDev1 pd8
+# K08 -- Ay Mon, Go Git It From Yer Flask
+# 2019-03-08
+
+from util import mongo
+from flask import session, Flask, render_template, request, redirect, url_for
+import os
 app = Flask(__name__)
+app.secret_key=os.urandom(32)# 32 bits of random data as a string
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+
+@app.route('/pokepage', methods=["POST"])
+def findpoke():
+    res = mongo.find_pokemon(request.form['pokeid'])
+    name = res['name']
+    img = res['img']
+    return render_template("pokeresult.html", pokename = name, imglink = img)
+
+@app.route('/changeip', methods=["POST"])
+def change():
+    mongo.build_db(request.form['ipnum'])
+    return redirect(url_for('home'))
+
+
+
 
 
 if __name__ == "__main__":
--- 08_mongosite/util/mongo.py
+++ 08_mongosite/util/mongo.py
@@ -10,62 +10,30 @@ import json
 
 from pprint import pprint
 
-SERVER_ADDR="46.101.242.188"
+SERVER_ADDR="68.183.120.187"
 client=MongoClient(SERVER_ADDR,27017)
 db=client.mongolia
 collection=db.pokedex
-print(client.test_database)
-print(db.list_collection_names())
+with open('pokedex.json') as f:
+    data = json.load(f)
+    collection.insert_many(data["pokemon"])
 
-'''
-Population Table for the United States, 1960
-This data set contains population data from the year 1960 in the United States. I am a bit confused because there is a "total" value for each age,
-but that is not equal to the sum of males and females. I used the sum of the 2 because I believed that that may give me more consitency.
-http://api.population.io/1.0/population/1960/United%20States/?format=json
-The import was done with the line $ sudo mongoimport -d macaroni --drop --file=pop.json --jsonArray
---jsonArray is necessary because our json file was stored as an array and attempting to import the file without this gave the error
-Failed: error unmarshaling bytes on document #0: JSON decoder out of sync - data changing underfoot?
-'''
 
-#
-# #[pprint(post) for post in collection.find({"age":"400"})]
-# male=0
-# female=0
-# malecnt=0
-# femalecnt=0
-#
-# record=collection.find()
-# for x in record:
-#     pprint(x)
-#     print(x["age"])
-#     male+=int(x["males"])*int(x["age"])
-#     female+=int(x["females"])*int(x["age"])
-#     malecnt+=int(x["males"])
-#     femalecnt+=int(x["females"])
-#
-# print("average female age in 1960:")
-# print(female/femalecnt)
-# print("average male age in 1960:")
-# print(male/malecnt)
-# print("average age in 1960:")
-# print((female+male)/(femalecnt+malecnt))
-#
-# # I kind of forget how to do percentiles but here goes nothing
-#
-# tot=femalecnt+malecnt
-# twentyfifth=int(.25*tot)
-# seventyfifth=int(.75*tot)
-# record=collection.find()
-# for x in record:
-#     twentyfifth-=int(x["males"])
-#     twentyfifth-=int(x["females"])
-#     seventyfifth-=int(x["females"])
-#     seventyfifth-=int(x["males"])
-#     if twentyfifth <=0:
-#         print("25th percentile age:")
-#         print(x["age"])
-#         twentyfifth+=100000000000000000000000000000000
-#     if seventyfifth <=0:
-#         print("75th percentile age:")
-#         print(x["age"])
-#         break
+def drop():
+    client.drop_database( "mongolia")
+
+def build_db(ipaddress):
+    drop()
+    client.close()
+    client = MongoClient(ipaddress, 27017)
+    db = client['mongolia']
+    collection = db['pokedex']
+    with open('pokedex.json') as f:
+        data = json.load(f)
+        collection.insert_many(data["pokemon"])
+
+def find_pokemon(name):
+    return collection.find_one({"name":name})
+
+
+#print(collection.find_one())
